451c36cff7075b042b57abaa1bc7fee6cae6b3a1
Prathemsh <prathmeshsonawane09538@gmail.com>
2023-04-14 07:32:10 +0530

    Dependancy installed and app.js created

---
+++ app.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+
+const port = process.env.PORT || 8000  ;
+
+//Starting a Server
+app.listen(port,() =>{
+    console.log(`app is running at ${port}`);
+});
