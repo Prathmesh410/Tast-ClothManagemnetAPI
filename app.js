883f64d525f9fcdaa56cc7a4c4380ba3da454af9
Prathemsh <prathmeshsonawane09538@gmail.com>
2023-04-14 19:37:19 +0530

    errors removed and auth file created

--- app.js
+++ app.js
@@ -6,10 +6,31 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
 
+//my routes
+// const authRoutes = require("./routes/auth");
+// const userRoutes = require("./routes/user");
+
+//routes
+// app.use("/api",authRoutes);
+// app.use("/api",userRoutes);
+
+//DB connection
+mongoose.connect(process.env.DATABASE,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex : true
+}).then(() =>{
+    console.log("DB Connected")
+}).catch(() => {
+    console.log("DB not Connected")
+});
+
+//running app
 const port = process.env.PORT || 8000  ;
-
-//Starting a Server
 app.listen(port,() =>{
     console.log(`app is running at ${port}`);
 });
